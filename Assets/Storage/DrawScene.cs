f45b2eb13875612d23267fba4d1a5dff026bb469
oneRain <onerain88@gmail.com>
2020-08-06 11:42:19 +0800

    feat: 增加客户端调用云函数示例

---
+++ Assets/Storage/DrawScene.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using LeanCloud.Storage;
+
+public class DrawScene : MonoBehaviour {
+    public Text resultText;
+
+    public async void OnDrawClicked() {
+        try {
+            Dictionary<string, object> results = await LCCloud.Run("draw");
+            List<object> heros = results["result"] as List<object>;
+            string info = $"恭喜获得：{string.Join(", ", heros)}";
+            resultText.text = info;
+            Debug.Log(info);
+        } catch (Exception e) {
+            Debug.LogError(e);
+        }    
+    }
+}
